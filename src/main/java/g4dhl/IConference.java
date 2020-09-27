fefefb3f62761a738272394aa2b000837aadd201
Harpreet Singh <hr557871@dal.ca>
2020-09-27 13:30:51 -0300

    Added Conference class and defined a Conference Interface. Also, added Test class for Conference.

---
+++ src/main/java/g4dhl/IConference.java
@@ -0,0 +1,12 @@
+package g4dhl;
+
+import java.util.ArrayList;
+
+public interface IConference {
+
+    String getConferenceName();
+    void setConferenceName(String conferenceName);
+
+    ArrayList<IDivision> getDivisions();
+    void setDivisions(ArrayList<IDivision> divisions);
+}
