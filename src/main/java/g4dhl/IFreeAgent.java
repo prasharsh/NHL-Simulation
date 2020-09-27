aae04b4018d7705f2af2599bb6657e1c7eb63570
Harpreet Singh <hr557871@dal.ca>
2020-09-27 13:49:34 -0300

    Added FreeAgent class and defined a FreeAgent Interface. Also, added Test class for FreeAgent.

---
+++ src/main/java/g4dhl/IFreeAgent.java
@@ -0,0 +1,10 @@
+package g4dhl;
+
+public interface IFreeAgent {
+    String getFreeAgentName();
+    void setFreeAgentName(String freeAgentName);
+    String getFreeAgentPosition();
+    void setFreeAgentPosition(String freeAgentPosition);
+    boolean isFreeAgentCaptain();
+    void setFreeAgentCaptain(boolean freeAgentCaptain);
+}
