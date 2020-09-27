f612f377f7c6ed6b7aa164cde8a55d8bbd940b38
Harpreet Singh <hr557871@dal.ca>
2020-09-27 13:57:21 -0300

    Added League class and defined a League Interface. Also, added Test class for League.

---
+++ src/main/java/g4dhl/ILeague.java
@@ -0,0 +1,15 @@
+package g4dhl;
+
+import java.util.ArrayList;
+
+public interface ILeague {
+
+    String getLeagueName();
+    void setLeagueName(String leagueName);
+
+    ArrayList<IConference> getConferences();
+    void setConferences(ArrayList<IConference> conferences);
+
+    ArrayList<IFreeAgent> getFreeAgents();
+    void setFreeAgents(ArrayList<IFreeAgent> freeAgents);
+}
