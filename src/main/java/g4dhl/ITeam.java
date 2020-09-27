d382e8ddeb80b6fee8461ecc5075d0e07673c787
Harpreet Singh <hr557871@dal.ca>
2020-09-27 13:07:00 -0300

    Added Team class and defined a Team Interface. Also, added Test class for Team.

---
+++ src/main/java/g4dhl/ITeam.java
@@ -0,0 +1,17 @@
+package g4dhl;
+
+import java.util.ArrayList;
+
+public interface ITeam {
+    String getTeamName();
+    void setTeamName(String teamName);
+
+    IGeneralManager getGeneralManager();
+    void setGeneralManager(IGeneralManager generalManager);
+
+    IHeadCoach getHeadCoach();
+    void setHeadCoach(IHeadCoach headCoach);
+
+    ArrayList<IPlayer> getPlayers();
+    void setPlayers(ArrayList<IPlayer> players);
+}
