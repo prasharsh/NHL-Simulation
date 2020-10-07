7b3a275638fc3136e84cb949739017ab254d151a
Harpreet Singh <hr557871@dal.ca>
2020-10-07 12:10:19 -0300

    Added getDivision and addDivision

--- src/main/java/g4dhl/IConference.java
+++ src/main/java/g4dhl/IConference.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 
 public interface IConference {
 
+    int getConferenceId();
+    void setConferenceId(int id);
     String getConferenceName();
     void setConferenceName(String conferenceName);
 
     ArrayList<IDivision> getDivisions();
+    IDivision getDivision(int index);
+    void addDivision(IDivision division);
     void setDivisions(ArrayList<IDivision> divisions);
 }
