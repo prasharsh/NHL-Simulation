4ab9a1e5ce6e4a116046812fca1d15c320266f2c
Harpreet Singh <hr557871@dal.ca>
2020-10-07 12:07:01 -0300

    Added getConference and addConference

--- src/main/java/g4dhl/ILeague.java
+++ src/main/java/g4dhl/ILeague.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 
 public interface ILeague {
 
+    int getLeagueId();
     String getLeagueName();
+
+    void setLeagueId(int leagueId);
     void setLeagueName(String leagueName);
 
     ArrayList<IConference> getConferences();
+    IConference getConference(int index);
+    void addConference(IConference conference);
     void setConferences(ArrayList<IConference> conferences);
 
     ArrayList<IFreeAgent> getFreeAgents();
+    IFreeAgent getFreeAgent(int index);
+    void addFreeAgent(IFreeAgent freeAgent);
     void setFreeAgents(ArrayList<IFreeAgent> freeAgents);
 }
