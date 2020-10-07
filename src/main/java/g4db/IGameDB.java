7b86ba1edfb87032c6e1b30b439d67940effd9ee
bdasari <bl200240@dal.ca>
2020-10-07 17:55:56 +0530

    #19 added missing import statements

--- src/main/java/g4db/IGameDB.java
+++ src/main/java/g4db/IGameDB.java
@@ -1,5 +1,11 @@
 package g4db;
 
+import g4dhl.IConference;
+import g4dhl.IDivision;
+import g4dhl.IGame;
+import g4dhl.ILeague;
+import g4dhl.ITeam;
+
 public interface IGameDB {
   void loadLeaguesFromDB(IGame game);
   void loadConferencesFromDB(ILeague league);
