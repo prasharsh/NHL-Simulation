f14abafba8ce2a4fd3f12a2233e238bd5395775b
Harpreet Singh <hr557871@dal.ca>
2020-10-08 02:37:40 -0300

    added business logic validations in Game class

--- src/main/java/g4dhl/Game.java
+++ src/main/java/g4dhl/Game.java
@@ -11,14 +11,37 @@ public class Game implements IGame, ILoadDataFromDB{
         this.leagues = new ArrayList<>();
     }
 
+    private boolean checkIfLeagueIsNull(ILeague league) {
+        return league == null;
+    }
+
+    private boolean checkIfLeagueNameIsEmptyOrNull(String leagueName) {
+        return leagueName == null || leagueName.trim().isEmpty();
+    }
+
+    private boolean checkIfLeagueNameAlreadyExists(String leagueName){
+        for(ILeague league: leagues){
+            if (league.getLeagueName().equals(leagueName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public ArrayList<ILeague> getLeagues() {
         return leagues;
     }
 
     @Override
-    public void addLeague(ILeague league){
+    public boolean addLeague(ILeague league){
+
+        if(checkIfLeagueIsNull(league)) return false;
+        if(checkIfLeagueNameIsEmptyOrNull(league.getLeagueName())) return false;
+        if(checkIfLeagueNameAlreadyExists(league.getLeagueName())) return false;
+
         leagues.add(league);
+        return true;
     }
 
     @Override
