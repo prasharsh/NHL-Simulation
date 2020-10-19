9f3f718355919086d89f1438651d21f779f10882
Harpreet Singh <hr557871@dal.ca>
2020-10-18 23:09:04 -0300

    Created Trading Package and defined trading class and the corresponding tests

---
+++ src/test/java/trading/TradingTest.java
@@ -0,0 +1,12 @@
+package trading;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TradingTest {
+
+    @Test
+    public void tradingTest(){
+       Assert.assertTrue("Hello", true);
+    }
+}
