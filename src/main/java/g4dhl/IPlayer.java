8c216e9bd8983a0fd7dfe1256b47ebb21e861521
Harpreet Singh <hr557871@dal.ca>
2020-10-18 23:21:07 -0300

    Added player stats to the player interface and player class

--- src/main/java/g4dhl/IPlayer.java
+++ src/main/java/g4dhl/IPlayer.java
@@ -4,9 +4,17 @@ public interface IPlayer {
 
     int getPlayerId();
     String getPlayerName();
+    int getPlayerSkating();
+    int getPlayerShooting();
+    int getPlayerChecking();
+    int getPlayerSaving();
 
     boolean setPlayerId(int playerId);
     boolean setPlayerName(String playerName);
+    boolean setPlayerSkating(int playerSkating);
+    boolean setPlayerShooting(int playerShooting);
+    boolean setPlayerChecking(int playerChecking);
+    boolean setPlayerSaving(int playerSaving);
 
     String getPlayerPosition();
     boolean setPlayerPosition(String playerPosition);
