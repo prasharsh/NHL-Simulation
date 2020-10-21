be0a618cba16934e22ab1a688b88f7696582c1cd
Harpreet Singh <hr557871@dal.ca>
2020-10-20 23:24:20 -0300

    Test to check player sorting on Strength

--- src/main/java/trading/Trading.java
+++ src/main/java/trading/Trading.java
@@ -1,8 +1,11 @@
 package trading;
 
+import g4dhl.IPlayer;
 import g4dhl.ITeam;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Trading {
 
@@ -12,23 +15,81 @@ public class Trading {
     private double randomAcceptanceChance = 0.05;
     private ArrayList<ITeam> teams;
 
-    public Trading(){
-        teams = new ArrayList<>();
-    }
-
-    public void startTrading(){
+    public void startTrading(ArrayList<ITeam> teams){
+        if (teams == null) return;
+        this.teams = new ArrayList<>(teams);
         checkForAiTradeOffers();
     }
 
-    public void checkForAiTradeOffers(){
+    private void checkForAiTradeOffers(){
 
         for (ITeam team: teams){
             if (team.getTeamCreatedBy().equals("import") && team.getLossPointCount() >= lossPoint){
                 if(Math.random() < randomTradeOfferChance){
-//                    generateAiTradeOffer(team, teams);
+                    generateAiTradeOffer(team, teams);
                 }
             }
         }
     }
+
+    private void generateAiTradeOffer(ITeam aiTeam, ArrayList<ITeam> teams) {
+
+        ArrayList<IPlayer> aiTeamWeakestPlayers = sortPlayersOnStrength(aiTeam.getPlayers(), maxPlayersPerTrade, true);
+
+        IPlayer aiWeakestPlayer = aiTeamWeakestPlayers.get(0);
+
+        for ( int i=1; i< aiTeamWeakestPlayers.size(); i++){
+
+            if (aiWeakestPlayer.getPlayerPosition().equals(aiTeamWeakestPlayers.get(i).getPlayerPosition())){
+                continue;
+            }
+            aiTeamWeakestPlayers.removeAll(aiTeamWeakestPlayers.subList(i, aiTeamWeakestPlayers.size()));
+            break;
+        }
+
+        for (ITeam opponentTeam: teams){
+
+            if (aiTeam.equals(opponentTeam)){
+                continue;
+            }
+
+            boolean isTradeAccepted = false;
+
+//            ArrayList<IPlayer> opponentTeamPlayersWithPosition = getPlayersWithPosition(opponentTeam.getPlayers(), aiWeakestPlayer.getPlayerPosition());
+//            ArrayList<IPlayer> opponentTeamStrongestPlayers = sortPlayersOnStrength(opponentTeamPlayersWithPosition, aiTeamWeakestPlayers.size(), false);
+//
+//            if (opponentTeam.getTeamCreatedBy().equals("import")){
+//                isTradeAccepted = generateAiTradeOfferToAi(aiTeam, aiTeamWeakestPlayers, opponentTeam, opponentTeamStrongestPlayers);
+//            }
+//            else if (opponentTeam.getTeamCreatedBy().equals("user")){
+//                isTradeAccepted = generateAiTradeOfferToUser(aiTeam, aiTeamWeakestPlayers, opponentTeam, opponentTeamStrongestPlayers);
+//            }
+
+            if (isTradeAccepted){
+                // swap the players, reset the counter for lossPointCount of aiTeam and return
+            }
+        }
+    }
+
+    public ArrayList<IPlayer> sortPlayersOnStrength(ArrayList<IPlayer> playersToBeSorted, int playersCount, final boolean ascending) {
+
+        ArrayList<IPlayer> players = new ArrayList<>(playersToBeSorted);
+
+        Collections.sort(players, new Comparator<IPlayer>() {
+
+            @Override
+            public int compare(IPlayer player1, IPlayer player2) {
+                if (ascending) return Double.compare(player1.getPlayerStrength(), player2.getPlayerStrength());
+                return Double.compare(player2.getPlayerStrength(), player1.getPlayerStrength());
+            }
+        });
+
+        if (playersCount >= playersToBeSorted.size()){
+            return players;
+        }
+
+        return (ArrayList<IPlayer>) players.subList(0, playersCount);
+    }
+
 
 }
--- src/test/java/trading/TradingTest.java
+++ src/test/java/trading/TradingTest.java
@@ -1,12 +1,67 @@
 package trading;
+import g4dhl.IPlayer;
+import g4dhl.Player;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 
 public class TradingTest {
 
     @Test
-    public void tradingTest(){
-       Assert.assertTrue("Hello", true);
+    public void sortPlayersOnStrengthInAscendingOrderTest(){
+
+        Trading trading = new Trading();
+        ArrayList<IPlayer> playersToBeSorted =  new ArrayList<>();
+        int playersCount = 2;
+
+        IPlayer player1 = new Player();
+        player1.setPlayerName("Rob");
+        player1.setPlayerSaving(5);
+        player1.setPlayerChecking(5);
+        player1.setPlayerShooting(5);
+        player1.setPlayerSkating(5);
+
+        IPlayer player2 = new Player();
+        player2.setPlayerName("Hawkey");
+        player2.setPlayerSaving(2);
+        player2.setPlayerChecking(2);
+        player2.setPlayerShooting(2);
+        player2.setPlayerSkating(2);
+
+        playersToBeSorted.add(player1);
+        playersToBeSorted.add(player2);
+
+        ArrayList<IPlayer> players = trading.sortPlayersOnStrength(playersToBeSorted, playersCount, true);
+        Assert.assertTrue("Players should be sorted in ascending order", players.get(0).getPlayerStrength() <= players.get(1).getPlayerStrength());
+    }
+
+    @Test
+    public void sortPlayersOnStrengthInDescendingOrderTest(){
+
+        Trading trading = new Trading();
+        ArrayList<IPlayer> playersToBeSorted =  new ArrayList<>();
+        int playersCount = 2;
+
+        IPlayer player1 = new Player();
+        player1.setPlayerName("Rob");
+        player1.setPlayerSaving(5);
+        player1.setPlayerChecking(5);
+        player1.setPlayerShooting(5);
+        player1.setPlayerSkating(5);
+
+        IPlayer player2 = new Player();
+        player2.setPlayerName("Hawkey");
+        player2.setPlayerSaving(2);
+        player2.setPlayerChecking(2);
+        player2.setPlayerShooting(2);
+        player2.setPlayerSkating(2);
+
+        playersToBeSorted.add(player1);
+        playersToBeSorted.add(player2);
+
+        ArrayList<IPlayer> players = trading.sortPlayersOnStrength(playersToBeSorted, playersCount, false);
+        Assert.assertTrue("Players should be sorted in descending order", players.get(0).getPlayerStrength() >= players.get(1).getPlayerStrength());
     }
 }
