4f6a3ecace81d3d798b8a123f015e3dec1f3d701
git config --global user.name prashantk <pr463475@dal.ca>
2020-10-28 18:57:35 -0300

    state machine integration

--- src/main/java/com/dal/dhl/states/AdvanceTime.java
+++ src/main/java/com/dal/dhl/states/AdvanceTime.java
@@ -1,6 +1,9 @@
 package com.dal.dhl.states;
 
 import java.sql.Date;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
 import com.dal.dhl.LeagueSimulation.TimeConcept;
 import com.dal.dhl.stateMachine.DHLStateMachine;
@@ -54,26 +57,40 @@ public class AdvanceTime implements IStateTransistion{
 		
 		stateMachine.setCurrState(stateMachine.getSimulateGame());
 		stateMachine.getCurrState().entry();
-		//if no
-		//check if trade deadline passed
-		//if yes
+		String[] date = stateMachine.getGame().getLeagues().get(0).getSimulationStartDate().toString().split("-");
+		int year = Integer.parseInt(date[0]);
+		Date tradeEndMonth = Date.valueOf(""+(year+1)+"-03-01");
+		LocalDate tradeEndDate = tradeEndMonth.toLocalDate()
+				.with( TemporalAdjusters.previous(DayOfWeek.MONDAY));
+		
+		Date lastTradeDate = Date.valueOf(tradeEndDate);
+		Date currDate = stateMachine.getGame().getLeagues().get(0).getCurrentDate();
+		if(currDate.compareTo(lastTradeDate) <0) {
+			stateMachine.setCurrState(stateMachine.getExecuteTrades());
+			stateMachine.getCurrState().entry();
+		}
+		else {
+			stateMachine.getGame();
+		}
 		stateMachine.setCurrState(stateMachine.getAging());
 		stateMachine.getCurrState().entry();
-		// is stanley cup winner determined
-		// if yes
-		stateMachine.setCurrState(stateMachine.getAdvanceNextSeason());
-		stateMachine.getCurrState().entry();
-		stateMachine.setCurrState(stateMachine.getPersist());
-		stateMachine.getCurrState().entry();
-
-		//if no
-		stateMachine.setCurrState(stateMachine.getPersist());
-		stateMachine.getCurrState().entry();
-		stateMachine.setCurrState(stateMachine.getAdvanceTime());
-		stateMachine.getCurrState().entry();
-		//if no
-		stateMachine.setCurrState(stateMachine.getExecuteTrades());
-		stateMachine.getCurrState().entry();
+		Date endOfSeason = Date.valueOf(""+(year+1)+"-06-01");
+		if(currDate.compareTo(endOfSeason)==1) {
+			stateMachine.setCurrState(stateMachine.getAdvanceNextSeason());
+			stateMachine.getCurrState().entry();
+			stateMachine.setCurrState(stateMachine.getPersist());
+			stateMachine.getCurrState().entry();
+			
+			System.out.println("end  of simulation");
+		}
+		else {
+			stateMachine.setCurrState(stateMachine.getPersist());
+			stateMachine.getCurrState().entry();
+			stateMachine.setCurrState(stateMachine.getAdvanceTime());
+			stateMachine.getCurrState().entry();
+			System.out.println("");
+		}
+		
 
 	}
 
