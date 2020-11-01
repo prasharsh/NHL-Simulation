0f605226f82b9814b72e178b211292daafbc05aa
git config --global user.name prashantk <pr463475@dal.ca>
2020-11-01 12:38:37 -0400

    changes as per submission instructions

--- src/main/java/com/datamodel/SimulateMatch.java
+++ src/main/java/com/datamodel/SimulateMatch.java
@@ -29,6 +29,7 @@ public class SimulateMatch {
                 teamStanding.setGamesPlayed(teamStanding.getGamesPlayed() + 1);
                 teamStanding.setGamesWon(teamStanding.getGamesWon() + 1);
                 teamStanding.setTotalPoints(teamStanding.getTotalPoints() + 2);
+                System.out.println(team.getTeamName()+" Vs "+opponentTeam.getTeamName() +" on "+game.getLeagues().get(0).getCurrentDate() +" was won by "+teamStanding.getTeam().getTeamName());
             } else if (teamStanding.getTeam().equals(opponentTeam) && isOpponentTeamLoss) {
                 teamStanding.setGamesPlayed(teamStanding.getGamesPlayed() + 1);
                 teamStanding.setGamesLost(teamStanding.getGamesLost() + 1);
@@ -38,6 +39,7 @@ public class SimulateMatch {
                 teamStanding.setGamesPlayed(teamStanding.getGamesPlayed() + 1);
                 teamStanding.setGamesWon(teamStanding.getGamesWon() + 1);
                 teamStanding.setTotalPoints(teamStanding.getTotalPoints() + 2);
+                System.out.println(team.getTeamName()+" Vs "+opponentTeam.getTeamName() +" on "+game.getLeagues().get(0).getCurrentDate() +" was won by "+teamStanding.getTeam().getTeamName());
             }
             if (teamStanding.getTeam().equals(team) && isOpponentTeamWin) {
                 teamStanding.setGamesPlayed(teamStanding.getGamesPlayed() + 1);
