5e0a19a518bc832f3075a9b4122f03715b294e6e
git config --global user.name prashantk <pr463475@dal.ca>
2020-11-17 14:59:16 -0400

    log4j set-up
    Signed-off-by: git config --global user.name prashantk <pr463475@dal.ca>

--- src/main/java/com/datamodel/leaguedatamodel/Main.java
+++ src/main/java/com/datamodel/leaguedatamodel/Main.java
@@ -1,9 +1,13 @@
 package com.datamodel.leaguedatamodel;
+import org.apache.log4j.Logger;
+
 import com.statemachine.IStateMachine;
 import com.statemachine.StateMachine;
 
 public class Main {
 
+	final static Logger logger = Logger.getLogger(Main.class);
+	
 	public static boolean isNullOrEmpty(String str) {
 		return str == null || str.trim().isEmpty();
 	}
@@ -12,8 +16,10 @@ public class Main {
 		String filePath = null;
 		try {
 			filePath = args[0];
+			logger.debug("args were passed");
 		} catch (ArrayIndexOutOfBoundsException ae) {
 			filePath = null;
+			logger.debug("No arguments passed");
 		}
 		IStateMachine stateMachine = new StateMachine(filePath);
 		stateMachine.start();
