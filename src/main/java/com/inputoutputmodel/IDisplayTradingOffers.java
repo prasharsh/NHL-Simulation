e0fe68563c417a62448b2b09ecfa67b9371436df
Deepkumar Dharmeshbhai Patel <dp827448@dal.ca>
2020-11-24 13:04:45 -0400

    Update IDisplayTradingOffers.java

--- src/main/java/com/inputoutputmodel/IDisplayTradingOffers.java
+++ src/main/java/com/inputoutputmodel/IDisplayTradingOffers.java
@@ -1,23 +1,12 @@
 package com.inputoutputmodel;
-
+import java.util.ArrayList;
 import com.datamodel.leaguedatamodel.IPlayer;
 import com.datamodel.leaguedatamodel.ITeam;
-<<<<<<< src/main/java/com/inputoutputmodel/IDisplayTradingOffers.java
-
-import java.util.ArrayList;
-=======
->>>>>>> src/main/java/com/inputoutputmodel/IDisplayTradingOffers.java
 
 public interface IDisplayTradingOffers {
-    void displayOfferToUser(ArrayList<IPlayer> offeredPlayers,ArrayList<IPlayer> consideredPlayers);
+	void displayOfferToUser(ArrayList<IPlayer> offeredPlayers, ArrayList<IPlayer> consideredPlayers);
 
-    boolean inputTradeAcceptRejectBooleanFromUser();
-
-<<<<<<< src/main/java/com/inputoutputmodel/IDisplayTradingOffers.java
-    void displayDraftOfferToUser(ITeam team,int teamPickRound,ArrayList<IPlayer> playersToTrade);
-=======
 	boolean inputTradeAcceptRejectBooleanFromUser();
 
     void displayDraftOfferToUser(ITeam team, int teamPickRound, ArrayList<IPlayer> playersToTrade);
->>>>>>> src/main/java/com/inputoutputmodel/IDisplayTradingOffers.java
 }
