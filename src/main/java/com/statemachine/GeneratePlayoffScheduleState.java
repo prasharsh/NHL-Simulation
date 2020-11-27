fc8ceb7edcaedffb00e07c61441f9f200798a0bf
git config --global user.name prashantk <pr463475@dal.ca>
2020-11-27 00:24:38 -0400

    output the league averages
    Signed-off-by: git config --global user.name prashantk <pr463475@dal.ca>

--- src/main/java/com/statemachine/GeneratePlayoffScheduleState.java
+++ src/main/java/com/statemachine/GeneratePlayoffScheduleState.java
@@ -1,6 +1,9 @@
 package com.statemachine;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import com.datamodel.leaguedatamodel.AbstractDataModelFactory;
-import com.datamodel.leaguedatamodel.GameSchedule;
 import com.datamodel.leaguedatamodel.IDataModelFactory;
 import com.datamodel.leaguedatamodel.IGameSchedule;
 
@@ -24,6 +27,15 @@ public class GeneratePlayoffScheduleState implements IState {
 	public IState doTask() {
 		IDataModelFactory dataModelFactory = AbstractDataModelFactory.getNewInstance();
 		IGameSchedule schedule = dataModelFactory.getGameSchedule();
+		HashMap<String, Double> averages = schedule.getScheduledGamesAverageStats(stateMachine.getGame().getLeagues().get(0).getGameSchedules());
+		Iterator it = averages.entrySet().iterator();
+		System.out.println("----------|Average Statistics|--------------");
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry)it.next();
+			System.out.println(pair.getKey() + " - " + String.format("%.2f", pair.getValue()));
+
+		}
+
 		schedule.schedulePlayoff(stateMachine.getGame(), stateMachine);
 		return stateMachine.getTraining();
 	}
