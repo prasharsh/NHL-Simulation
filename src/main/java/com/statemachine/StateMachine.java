4c2b997f61c22e81d5ed7326fc30d2c5e1c20b13
git config --global user.name prashantk <pr463475@dal.ca>
2020-11-28 12:32:15 -0400

    removing spaces
    Signed-off-by: git config --global user.name prashantk <pr463475@dal.ca>

--- src/main/java/com/statemachine/StateMachine.java
+++ src/main/java/com/statemachine/StateMachine.java
@@ -20,7 +20,6 @@ public class StateMachine implements IStateMachine {
     }
 
     private void transistionToState(IState toState) {
-        
         currentState = toState;
         currentState.entry();
     }
